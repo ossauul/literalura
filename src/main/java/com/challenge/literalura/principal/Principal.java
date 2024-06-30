d7586c1a79c51f282131a180cc826a7e25d59f50
ossauul <ossaul13@outlook.com>
2024-06-30 09:41:09 -0600

    Prueba de funcionamiento ConsumoAPI Se agrego un pequeño metodo en la clase Principal, asi se comprobo el funcionamiento correcto

--- src/main/java/com/challenge/literalura/principal/Principal.java
+++ src/main/java/com/challenge/literalura/principal/Principal.java
@@ -8,17 +8,17 @@ import java.util.Scanner;
 public class Principal {
     private Scanner teclado = new Scanner(System.in);
     private ConsumoAPI consumoApi = new ConsumoAPI();
-    private final String URL_BASE = "https://gutendex.com/books?";
-    private final String BUSQUEDA_LIBRO = "search=";
+    private final String URL_BASE = "https://gutendex.com/books/";
+    private final String BUSQUEDA_LIBRO = "?search=";
     private final String BUSQUEDA_INICIO_FECHA1 = "author_year_start=";
     private final String BUSQUEDA_FIN_FECHA = "author_year_end=";
 
 //Cambiar void a clase de Datos
     public void getDatosSerie() {
         //System.out.println("Escribe el nombre del libro a buscar que deseas buscar");
-        //var nombreLibro = teclado.nextLine();
-        var json = consumoApi.obtenerDatos("http://gutendex.com/books?search=pride%20and%20prejudice");
-        //System.out.println(URL_BASE + BUSQUEDA_LIBRO + nombreLibro.replace(" ", "%20"));
+        var nombreLibro = teclado.nextLine();
+        var json = consumoApi.obtenerLibros(URL_BASE + BUSQUEDA_LIBRO + nombreLibro.replace(" ", "+"));
+        System.out.println(URL_BASE + BUSQUEDA_LIBRO + nombreLibro.replace(" ", "+"));
         System.out.println(json);
         //DatosSerie datos = conversor.obtenerDatos(json, DatosSerie.class);
         //return datos;
--- src/main/java/com/challenge/literalura/service/ConsumoAPI.java
+++ src/main/java/com/challenge/literalura/service/ConsumoAPI.java
@@ -7,21 +7,43 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsumoAPI {
-    public String obtenerDatos(String url){
+    public String obtenerLibros(String url){
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    /* public String obtenerDatos(String url){
+        HttpClient client = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .build();
+        System.out.println("Respuesta cliente" +client);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        System.out.println("Respuesta request"+ request);
         HttpResponse<String> response = null;
         try {
             response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Respuesta response" + response);
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         String json = response.body();
+        System.out.println("json"+json);
         return json;
-    }
+    }*/
 }
