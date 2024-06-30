4e27519afe7021aebc420da7a26d5eb24aa2861c
ossauul <ossaul13@outlook.com>
2024-06-29 22:06:21 -0600

    Creacion clase Principal

---
+++ src/main/java/com/challenge/literalura/principal/Principal.java
@@ -0,0 +1,28 @@
+package com.challenge.literalura.principal;
+
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
+import com.challenge.literalura.service.ConsumoAPI;
+
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner teclado = new Scanner(System.in);
+    private ConsumoAPI consumoApi = new ConsumoAPI();
+    private final String URL_BASE = "https://gutendex.com/books?";
+    private final String BUSQUEDA_LIBRO = "search=";
+    private final String BUSQUEDA_INICIO_FECHA1 = "author_year_start=";
+    private final String BUSQUEDA_FIN_FECHA = "author_year_end=";
+
+//Cambiar void a clase de Datos
+    public void getDatosSerie() {
+        //System.out.println("Escribe el nombre del libro a buscar que deseas buscar");
+        //var nombreLibro = teclado.nextLine();
+        var json = consumoApi.obtenerDatos("http://gutendex.com/books?search=pride%20and%20prejudice");
+        //System.out.println(URL_BASE + BUSQUEDA_LIBRO + nombreLibro.replace(" ", "%20"));
+        System.out.println(json);
+        //DatosSerie datos = conversor.obtenerDatos(json, DatosSerie.class);
+        //return datos;
+    }
+
+
+}
