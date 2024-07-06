ac5822d65c7220a2d49a14300cf51e25dca5412c
ossauul <ossaul13@outlook.com>
2024-07-06 16:11:54 -0600

    Creacion de Record DatosResultados Record creado para capturar el arreglo "results" de la API (formato JSON)

---
+++ src/main/java/com/challenge/literalura/model/DatosResultados.java
@@ -0,0 +1,10 @@
+package com.challenge.literalura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosResultados(@JsonAlias("results") List<DatosLibro> libros) {
+}
