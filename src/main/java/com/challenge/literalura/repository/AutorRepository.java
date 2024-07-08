aa335091dbb99a77826b0ef4d68ed1f52dbd207c
ossauul <ossaul13@outlook.com>
2024-07-07 20:52:21 -0600

    Modificacion de AutorRepository Agregacion de metodos busquedaAutores, busquedaAutoresAnio, busquedaAutoresAnios, busquedaNombre, busquedaPorNacimiento, busquedaPorFallecimiento para la interaccion con la base de datos

--- src/main/java/com/challenge/literalura/repository/AutorRepository.java
+++ src/main/java/com/challenge/literalura/repository/AutorRepository.java
@@ -2,11 +2,30 @@ package com.challenge.literalura.repository;
 
 import com.challenge.literalura.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AutorRepository extends JpaRepository<Autor, Long> {
-    Optional<Autor> findByNombreContainsIgnoreCase(String Autor);
+    @Query("SELECT a FROM Autor a JOIN FETCH a.libro")
+    List<Autor> busquedaAutores();
+
+    @Query("SELECT a FROM Autor a JOIN FETCH a.libro l WHERE a.fechaFallecimiento <= :anio")
+    List<Autor> busquedaAutoresAnio(int anio);
+
+    @Query("SELECT a FROM Autor a JOIN FETCH a.libro l WHERE a.fechaFallecimiento >= :anio AND a.fechaFallecimiento <= :anio2")
+    List<Autor> busquedaAutoresAnios(@Param("anio") int anio, @Param("anio2") int anio2);
+
+    @Query("SELECT a FROM Autor a JOIN FETCH a.libro WHERE a.nombre ILIKE %:nombre%")
+    List<Autor> busquedaNombre(String nombre);
+
+    @Query("SELECT a FROM Autor a JOIN FETCH a.libro WHERE a.fechaNacimiento = :fecha")
+    List<Autor> busquedaPorNacimiento(int fecha);
+
+    @Query("SELECT a FROM Autor a JOIN FETCH a.libro WHERE a.fechaFallecimiento = :fecha")
+    List<Autor> busquedaPorFallecimiento(int fecha);
 
 }
