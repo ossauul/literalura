b5401fdf81533b75e76051d87ab4faa9ecad026d
ossauul <ossaul13@outlook.com>
2024-07-07 20:59:23 -0600

    Modificacion de LibroRepository Agregacion de metodos busquedaPorIdioma,busquedasLibros, topDiezLibros

--- src/main/java/com/challenge/literalura/repository/LibroRepository.java
+++ src/main/java/com/challenge/literalura/repository/LibroRepository.java
@@ -1,17 +1,32 @@
 package com.challenge.literalura.repository;
 
+import com.challenge.literalura.model.Autor;
+import com.challenge.literalura.model.Lenguajes;
 import com.challenge.literalura.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LibroRepository extends JpaRepository<Libro, Long> {
     //Optional<Libro> findByTituloContainsIgnoreCase(String titulo);
 
-    @Query("SELECT l FROM Libro l WHERE LOWER(l.titulo) = LOWER(:titulo)")
+    @Query("SELECT l.titulo FROM Libro l WHERE LOWER(l.titulo) = LOWER(:titulo)")
     Optional<Libro> findByTituloEqualsIgnoreCase(@Param("titulo") String titulo);
+
+    @Query("SELECT l FROM Libro l JOIN FETCH l.autor WHERE l.idiomas = %:idiomas%")
+    List<Libro> busquedaPorIdioma(Lenguajes idiomas);
+
+    @Query("SELECT l FROM Libro l JOIN FETCH l.autor")
+    List<Libro> busquedasLibros();
+
+    @Query("SELECT l FROM Libro l JOIN FETCH l.autor order by l.descargas DESC LIMIT 10")
+    List<Libro> topDiezLibros();
+
+
+
 
 
 
